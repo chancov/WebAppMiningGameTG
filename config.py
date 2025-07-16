ceb37a4cdd08af0c2ca8d1650b4763a1374b0ac5
home-dev <97825620+chancov@users.noreply.github.com>
2025-07-16 13:15:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-TELEGRAM_BOT_TOKEN = '7692584580:AAEu8Av3XZJW1RrSApObioxu4Ujckp6Cd8g'
-FLASK_SECRET_KEY = '123456789'
-WEBAPP_URL = 'https://861b-94-179-156-91.ngrok-free.app/user'  # URL веб-приложения 
+TELEGRAM_BOT_TOKEN = '7770131730:AAFQD24h8swrBvOUP-h-Lpr55d9Yd7Sc58Q'
+FLASK_SECRET_KEY = '538244164'
+WEBAPP_URL = 'https://webappmininggametg.onrender.com/templates/index.html'  # URL веб-приложения 
